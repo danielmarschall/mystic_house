f8e1d2357f4ee1cd8c2b5f340dadafd8bdb49b75
Daniel Marschall <info@daniel-marschall.de>
2019-05-02 11:11:49 +0000

    Initial release to SVN
    git-svn-id: https://svn.viathinksoft.com/svn/mystic_house/trunk@2 94cf984c-3bee-41a6-b22f-b5756762db58

---
+++ House.dpr
@@ -0,0 +1,26 @@
+program House;
+
+{$R *.res}
+
+uses
+  Windows,
+  SysUtils,
+  Dialogs,
+  ShellAPI;
+
+var
+  exe, params, dir: string;
+
+begin
+  exe := ExtractFilePath(ParamStr(0))+'gplay32.exe';
+  params := '/N'+ChangeFileExt(ParamStr(0), '');
+  dir := ExtractFilePath(ParamStr(0));
+
+  if not FileExists(exe) then
+  begin
+    ShowMessageFmt('Error: %s not found!', [exe]);
+    Exit;
+  end;
+
+  ShellExecute(0, 'open', PChar(exe), PChar(params), PChar(dir), SW_NORMAL);
+end.
